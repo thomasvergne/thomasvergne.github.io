a1beefa85ccd0620a2623d3e96dffcec6f244fed
Thomas Vergne <contact@thomas-vergne.fr>
2024-10-20 23:10:27 +0200

    build(astro): added file compression in astro build

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,10 @@ import tailwind from '@astrojs/tailwind';
 
 import icon from 'astro-icon';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://thomas-vergne.fr',
-    integrations: [mdx(), sitemap(), tailwind(), icon()],
+    integrations: [mdx(), sitemap(), tailwind(), icon(), compress()],
 });
