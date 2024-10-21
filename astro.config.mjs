38b9d50d3161cd6ad0798bffc0102bf41489788a
Thomas Vergne <contact@thomas-vergne.fr>
2024-10-21 11:46:30 +0200

    build(astro): added bonzai syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,27 @@ import icon from 'astro-icon';
 
 import compress from 'astro-compress';
 
+import ShikiTheme from 'shiki/themes/github-dark-default.mjs';
+
+import bonzaiTextMate from './public/bonzai.tmLanguage.json' assert { type: 'json' };
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://thomas-vergne.fr',
     integrations: [mdx(), sitemap(), tailwind(), icon(), compress()],
+    markdown: {
+      shikiConfig: {
+        theme: {
+          ...ShikiTheme,
+          bg: '#171717',
+          fg: '#d6d3d1',
+        },
+        langs: [
+          {
+            ...bonzaiTextMate,
+            aliases: ['bonzai'],
+          }
+        ]
+      }
+    }
 });
